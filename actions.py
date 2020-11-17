ec10a906e6c5e1a3bb772b5d90c21e8ae01695dd
Lautaro Bernardini <lauerber@hotmail.com.ar>
2020-11-17 10:34:33 -0300

    fixes 17/11 bot 3

--- actions.py
+++ actions.py
@@ -138,11 +138,11 @@ def send_message(mensaje="", archivo="", celular="", masive=False, driver=None, 
                     e = None
                     while not e:
                         try:
-                            driver.find_element_by_xpath(selectors["search"]).click()
-                            driver.find_element_by_xpath(selectors["preview"]).click()
                             message = driver.find_element_by_xpath(selectors["message_attached"])
                             e = True
                         except:
+                            driver.find_element_by_xpath(selectors["search"]).click()
+                            driver.find_element_by_xpath(selectors["preview"]).click()
                             time.sleep(2)
                 else:
                     # Enviar archivo
