1b4c1dc2bbd0f31310beae058f5b8e35afa0dda9
Lautaro Bernardini <lauerber@hotmail.com.ar>
2021-07-12 18:25:59 -0300

    fix update

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ AUTO = "NO"
 # Datos del server
 SERVER_URL = "https://apicloud.com.ar/thread-"#"http://localhost:8060/thread-"#
 FILE_SERVER = "http://apicloud.com.ar:8080"#"http://localhost:8000"#
-FILE_SERVER_2 = "http://localhost:8000"#"https://apicloud.com.ar"#
+FILE_SERVER_2 = "https://apicloud.com.ar"#"http://localhost:8000"#
 
 # Variables de ejecución
 OS_SLASH = "/" # / -> linux \\ -> windows
