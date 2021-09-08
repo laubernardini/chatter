27c0d24b4f177a1d3404cfa6c79a1a4579fb5070
Lautaro Bernardini <lauerber@hotmail.com.ar>
2021-09-08 10:20:54 -0300

    celular por consola

--- run.py
+++ run.py
@@ -1,28 +1,29 @@
 import asyncio
 from os import system
 import bot
-import getopt
+#import getopt
 import sys
 
 from main import start
 from apis import status
 
-argv = sys.argv[1:]
-opts, args = getopt.getopt(argv, 'b:c:', ['foperand', 'soperand'])
-if len(opts) == 0:# or len(opts) > 1:
-    print ('Falta un parámetro: -b <BOT_PK>')
-    #exit()
-else:
-    bot.BOT_PK = opts[0][1]
-    if len(opts) == 2:
-        try:
-            phone = opts[1][1]
-            if phone and phone != "":
-                bot.PHONE = phone
-        except:
-            pass
-    bot.VERSION = "3.0"
-    print(f"PK: {bot.BOT_PK}, PHONE: {bot.PHONE}")
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-b","--bot", help="Bot PK")
+parser.add_argument("-c", "--celular", help="Celular")
+args = parser.parse_args()
+
+#argv = sys.argv[1:]
+opts = []#, args = getopt.getopt(argv, 'b::c::', ['foperand'])
+if args.bot:
+    bot.BOT_PK = args.bot
+if args.celular:
+    phone = args.celular
+    if phone and phone != "":
+        bot.PHONE = phone
+        
+bot.VERSION = "3.0"
+print(f"PK: {bot.BOT_PK}, PHONE: {bot.PHONE}")
 
 bot.DOWNLOAD_PATH = bot.DOWNLOAD_PATH + str(bot.BOT_PK) + bot.OS_SLASH
 
