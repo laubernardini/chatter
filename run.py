c476a51bab7c1a6b2d380247c9fb2dc483d25d07
Lautaro Bernardini <lauerber@hotmail.com.ar>
2022-03-24 13:59:59 -0300

    multi server

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-b","--bot", help="Bot PK")
 parser.add_argument("-c", "--celular", help="Celular")
+parser.add_argument("-s", "--server", help="Server")
 args = parser.parse_args()
 
 if args.bot:
@@ -19,7 +20,11 @@ if args.celular:
     phone = args.celular
     if phone and phone != "":
         bot.PHONE = phone
-        
+if args.server == 'cat' or args.server == 'CAT':
+    bot.SERVER_URL = "https://cat-technologies.apicloud.com.ar/thread-"
+    bot.FILE_SERVER = "https://cat-technologies.apicloud.com.ar"
+    bot.FILE_SERVER_2 = "http://cat-technologies.apicloud.com.ar:8080"
+
 bot.VERSION = "3.0"
 print(f"PK: {bot.BOT_PK}, PHONE: {bot.PHONE}")
 
